b9075ef54892fc2db65a17bca7b4f7a86e937c2e
archbober <git@archbober.com>
2026-01-22 05:53:56 +0100

    tfgh

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
-import { handlerChirpsValidate } from "./api/validatechirp.js";
+import { handlerChirpsCreate } from "./api/chirps.js";
 import { handlerUsersCreate } from "./api/users.js";
 import {
   middlewareLogResponse,
@@ -36,9 +36,9 @@ app.post("/admin/metrics", (req, res, next) => {
 app.post("/admin/reset", (req, res, next) => {
   Promise.resolve(handlerReset(req, res)).catch(next)}
 );
-app.post("/api/validate_chirp", (req, res, next) => {
-  Promise.resolve(handlerChirpsValidate(req, res)).catch(next)}
-);
+app.post("/api/chirps", (req, res, next) => {
+  Promise.resolve(handlerChirpsCreate(req, res)).catch(next);
+});
 app.post("/api/users", (req, res, next) => {
   Promise.resolve(handlerUsersCreate(req, res)).catch(next);
 });
